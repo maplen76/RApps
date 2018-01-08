d4336c7ae9841608005b246768335933a14b2b40
Alex <jingwang512@gmail.com>
2018-01-08 16:16:07 +0800

    Create linear_regression.R

---
+++ linear_regression.R
@@ -0,0 +1,34 @@
+# Linear Regression
+library(MASS)
+library(ISLR)
+data(Boston) # load Boston data
+fix(Boston)
+
+# medv as the respnse and lastat as the predictor
+lm.fit <- lm(medv ~ lstat, data = Boston)
+
+summary(lm.fit) # summary of the linear regression modle
+coef(lm.fit) # coef to get the coefficients
+confint(lm.fit) # confint to get the confidence interval
+predict(object = lm.fit, newdata = data.frame(lstat = c(5,10,15)), interval = "confidence") # predict function to produce confidence intervals and prediction intervals for the prediction
+
+plot(x = Boston$lstat, y = Boston$medv)
+abline(lm.fit)
+
+# use ggplot method to generate the plot with confidence interval
+ggplot(data = Boston, mapping = aes(x = lstat, y = medv)) + 
+    geom_point() +
+    scale_y_continuous(limits = c(0,NA)) +
+    geom_smooth(method = "lm", se = T)
+
+
+# disgnostic plots
+par(mfrow = c(2,2)) # par is set to view all 4 plots together
+plot(lm.fit) # call plot with the model to generate diagnostics plots
+
+plot(predict(lm.fit), residuals(lm.fit))
+plot(predict(lm.fit), rstudent(lm.fit))
+
+plot(hatvalues(lm.fit))
+
+
