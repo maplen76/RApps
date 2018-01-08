eb9f92d2cad1bf31cf824a6a5d3216ebb5ffe0df
Alex <jingwang512@gmail.com>
2018-01-08 17:31:07 +0800

    Update linear_regression.R

--- linear_regression.R
+++ linear_regression.R
@@ -4,8 +4,10 @@ library(ISLR)
 data(Boston) # load Boston data
 fix(Boston)
 
+# Simple Linear Regression
+
 # medv as the respnse and lastat as the predictor
-lm.fit <- lm(medv ~ lstat, data = Boston)
+lm.fit <- lm(medv ~ lstat, data = Boston) # model syntax, y ~ x
 
 summary(lm.fit) # summary of the linear regression modle
 coef(lm.fit) # coef to get the coefficients
@@ -31,4 +33,23 @@ plot(predict(lm.fit), rstudent(lm.fit))
 
 plot(hatvalues(lm.fit))
 
+# Multiple Linear Regression
+lm.fit <- lm(medv ~ lstat + age, data = Boston)
+lm.fit <- lm(medv ~ ., data = Boston) # perform regression using all of variables
+summary(lm.fit)
+lm.fit1 <- lm(medv ~ .-age, data = Boston) # perform regression using all of variables but one
 
+# interaction terms
+summary(lm(medv ~ lstat * age, data = Boston)) # lstat * age inludes lstat, age, lstat*age as predictors
+
+lm.fit2 <- lm(medv ~ lstat + I(lstat^2), data = Boston) # function I() is needed to perform Non-liner transforamtion
+lm.fit <- lm(medv ~ lstat, data = Boston)
+summary(lm.fit2)
+anova(lm.fit, lm.fit2)
+
+lm.fit5 <- lm(medv ~ poly(lstat, 5), data = Boston) # use polynomial model
+summary(lm.fit5)
+
+
+# for Qualitative Predictors, R will generate dummy variables
+data(Carseats)
