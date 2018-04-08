c15729c1182d6abc916236e0bb5d7c3c52a64b37
Alex <jingwang512@gmail.com>
2018-04-08 15:34:12 +0800

    Create sendMailwith_sendmailR.R

---
+++ sendMailwith_sendmailR.R
@@ -0,0 +1,16 @@
+library(sendmailR)
+recipient <- c("xxx@xxx.com", "xxx@xxx.com")
+
+from <- "xxx@xxxcom"
+to <- recipient
+bcc <- "xxx@xxx.com"
+mailControl = list(smtpServer = "xxxxxx")
+
+body <- paste0("<html>", 'test', "</html>")
+subject <- 'test'
+
+to_ <- "xxx@xxxcom"
+msg <- mime_part(body)
+msg[["headers"]][["Content-Type"]] <- "text/html"
+        
+sendmail(from=from, to=to_, bcc = bcc, subject=subject, msg=msg, control=mailControl)        
