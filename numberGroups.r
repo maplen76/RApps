7e2c286469ec60eb14744d0cb2a4dfec72727b20
Alex <jingwang512@gmail.com>
2018-12-03 09:26:40 +0800

    Update numberGroups.r

--- numberGroups.r
+++ numberGroups.r
@@ -1,14 +1,29 @@
 # categorize numbers into different groups
 
-cat <- function(x, lower = 0, start = 0, upper, by = 10,
-                sep = "-", above.char = "+", below.char = "-") {
+cat <- function(x, lower = 0, start = 0, upper, by = 2,
+                sep = "-", above.char = "+", below.char = "-", Suff = "Month") {
     
-    labs <- c(paste(lower,below.char,start-1, sep = ""),
-              paste(seq(start, upper - by, by = by),
-                    seq(start + by - 1, upper - 1, by = by),
-                    sep = sep),
-              paste(upper, above.char, sep = ""))
+    # start have to be greater than lower
+    labs <- c(
+        
+        # build left beginning
+        paste(lower, below.char, start, " ", Suff, sep = ""),
+        
+        # build middle intervals
+        paste(
+            seq(start + 1, upper - by + 1, by = by),
+            paste0(seq(start + by, upper + 1, by = by), " ", Suff),
+            sep = sep
+        ),
+        
+        # build reight endpoint
+        paste(upper + 1, above.char, " ", Suff, sep = "")
+    )
     
-    cut(floor(x), breaks = c(lower, seq(start, upper, by = by), Inf),
-        right = FALSE, labels = labs) 
+    cut(
+        floor(x), 
+        breaks = c(lower, seq(start, upper, by = by), Inf),
+        right = FALSE, 
+        labels = labs
+    ) 
 }
