5b44ba79d331e4b96922dbd9dc262840bde8daae
Alex <jingwang512@gmail.com>
2021-11-23 16:49:24 +0800

    Update scrape_metaCriticScore.R

--- scrape_metaCriticScore.R
+++ scrape_metaCriticScore.R
@@ -10,22 +10,26 @@ scrape_text <- function(htmlnodes, css_selector){
     html_text2()
   
   if (length(text) == 0) {
-    text = NA
+    text <- NA
   }
   
   return(text)
 }
 
-# build empty dataframe to store data
+# build empty data frame to store data
 meta_games_all <- data.frame()
 
-for (i in 0:190) {
+for (i in 36:36) {
+
   page = paste0("https://www.metacritic.com/browse/games/score/metascore/all/all/filtered?page=", as.character(i))
   meta_html <- read_html(page)
   
+  # extract title names
   title_name <- meta_html |> 
     html_nodes(css = ".title  h3") |>
     html_text2()
+  
+  # extract link of each game
   title_url <- meta_html |> 
     html_nodes(css = "td.clamp-image-wrap > a") |>
     html_attr("href")
@@ -45,44 +49,50 @@ for (i in 0:190) {
   
   for (j in 1:length(title_url_1)) {
     # game detail
-    game <- read_html(title_url_1[j])
     
-    # publisher
-    publisher[j] = scrape_text(game, "li.summary_detail.publisher > span.data" )
-    
-    # developer
-    developer[j] <- scrape_text(game, "li.summary_detail.developer > span.data" )
-
-    # genre
-    genre[j] <- scrape_text(game, "li.summary_detail.product_genre")
-
-    # nb players
-    nbplayers[j] <- scrape_text(game, "li.summary_detail.product_players")
-
-    # rating
-    rating[j] <- scrape_text(game, "li.summary_detail.product_rating > span.data")
-
-    # platform
-    platform[j] <- scrape_text(game,"div.product_title > span.platform")
-
-    # platform_others
-    platform_other[j] <- scrape_text(game,"li.summary_detail.product_platforms")
-
-    # release date
-    releasedate[j] <- scrape_text(game, "li.summary_detail.release_data > span.data")
-
-    # meta score
-    metaScore[j] <- scrape_text(game, "div.details.main_details > div > div > a > div > span") 
-
-    # user score
-    userScore[j] <- scrape_text(game, "div.details.side_details > div:nth-child(1) > div > a > div")
-
-    # description
-    title_description <- scrape_text(game, "div.details.main_details > ul > li > span.data > span > span.blurb.blurb_expanded")
-    if ( is.na(title_description) ) {
-      title_description <-scrape_text(game, "div.details.main_details > ul > li > span.data > span")
-    }
-    description[j] <- title_description
+    try(
+      {
+        game <- read_html(title_url_1[j])
+        
+        # publisher
+        publisher[j] = scrape_text(game, "li.summary_detail.publisher > span.data" )
+        
+        # developer
+        developer[j] <- scrape_text(game, "li.summary_detail.developer > span.data" )
+        
+        # genre
+        genre[j] <- scrape_text(game, "li.summary_detail.product_genre")
+        
+        # nb players
+        nbplayers[j] <- scrape_text(game, "li.summary_detail.product_players")
+        
+        # rating
+        rating[j] <- scrape_text(game, "li.summary_detail.product_rating > span.data")
+        
+        # platform
+        platform[j] <- scrape_text(game,"div.product_title > span.platform")
+        
+        # platform_others
+        platform_other[j] <- scrape_text(game,"li.summary_detail.product_platforms")
+        
+        # release date
+        releasedate[j] <- scrape_text(game, "li.summary_detail.release_data > span.data")
+        
+        # meta score
+        metaScore[j] <- scrape_text(game, "div.details.main_details > div > div > a > div > span") 
+        
+        # user score
+        userScore[j] <- scrape_text(game, "div.details.side_details > div:nth-child(1) > div > a > div")
+        
+        # description
+        title_description <- scrape_text(game, "div.details.main_details > ul > li > span.data > span > span.blurb.blurb_expanded")
+        if ( is.na(title_description) ) {
+          title_description <-scrape_text(game, "div.details.main_details > ul > li > span.data > span")
+        }
+        description[j] <- title_description
+      },
+    silent = TRUE
+    )
     
   }
   
@@ -105,3 +115,13 @@ for (i in 0:190) {
   meta_games_all <- rbind.data.frame(meta_games_all, mata_games)
 
 }
+
+# write csv files
+meta_games_all <- meta_games_all |>
+  mutate(genre = str_replace_all(string = genre, pattern = "Genre\\(s\\): ", replacement = ""),
+         nbplayers = str_replace_all(string = nbplayers, pattern = "# of players: ", replacement = "")
+         )
+
+write.csv(x = meta_games_all, file = "meta_score.csv")
+
+
